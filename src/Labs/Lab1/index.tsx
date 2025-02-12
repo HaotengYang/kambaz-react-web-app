2cfe261135f769932d0f2c1c950c880cb55cd860
yangyang <yanghaoteng0921@gmail.com>
2025-02-12 11:58:17 -0800

    Final submission for Kambaz project

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Lab1 = () => {
-  return <div>Lab1 Placeholder Page</div>;
+  return (
+    <div>
+      <h2>Lab 1</h2>
+      <p>This is Lab 1 page.</p>
+    </div>
+  );
 };
 
 export default Lab1;
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Lab2 = () => {
-  return <div>Lab2 Placeholder Page</div>;
+  return (
+    <div>
+      <h2>Lab 2</h2>
+      <p>This is Lab 2 page.</p>
+    </div>
+  );
 };
 
 export default Lab2;
