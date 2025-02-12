5747d98201d27f7e0c48fe2631a43795d7d1362d
yangyang <yanghaoteng0921@gmail.com>
2025-02-12 09:50:22 -0800

    a2 CSS and Bootstrap

--- src/Labs/Lab2/CssSelectors.tsx
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-
-export default function CssSelectors() {
-  return (
-    <div className="wd-css-class">
-      <h3>This is styled with an external CSS class</h3>
-    </div>
-  );
-}
--- src/Labs/Lab2/Styling.tsx
+++
@@ -1,9 +0,0 @@
-export default function Styling() {
-  return (
-    <div>
-      <h2 style={{ color: "blue", backgroundColor: "lightgray" }}>
-        This is a heading with inline CSS
-      </h2>
-    </div>
-  );
-}
